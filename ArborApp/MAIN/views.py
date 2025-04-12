805143ccdf41056d792811635d07a38fd8564c9f
RJM <Rmorr2001@gmail.com>
2025-04-12 17:36:44 -0500

    We have two pages now

--- ArborApp/MAIN/views.py
+++ ArborApp/MAIN/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'MAIN/home.html')
+
+def request(request):
+  return render(request, 'MAIN/request.html')
